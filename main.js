b7de6c6af7fd86a02341b925f327e8862bc58c7c
George Chearswat <chearswa@usc.edu>
2012-10-29 22:17:52 -0700

    First upload, adding class does not work yet... :(

---
+++ main.js
@@ -0,0 +1,18 @@
+$('ul').on('click', 'a', function() {
+	var $handle = $(this).attr('id');
+	$('#tweets').html('<img src="ajax-loader.gif" />');
+	$.ajax({
+		url: 'twitter-JSON.php',
+		type: 'GET',
+		data: {
+			name: $handle
+		},
+		success: function(msg) {
+			$('#tweets').html(msg);
+		}
+	});
+});
+
+$('#tweets').on('click', 'li', function() {
+	$(this).addclass("read");
+});
